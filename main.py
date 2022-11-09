1f2a7553d2ffb8d9ccaf0a8948983b93ebeb3e6c
Ritwik Srivastava <ritwikdbz@gmail.com>
2022-11-09 10:22:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Number List Generator!!!\n\n")
+start_num = int(input("Enter a starting number: "))
+end_num = int(input("Enter an ending number: "))
+incrementor = int(input("Enter an increment: "))
+
+for _ in range(start_num,end_num,incrementor):
+  print( _)
